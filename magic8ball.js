219ba109692355501ec40c9157037221487e9e85
MAGDALA-CODING <123142090+MAGDALA-CODING@users.noreply.github.com>
2023-04-05 06:50:14 -0400

    Create magic8ball.js

---
+++ magic8ball.js
@@ -0,0 +1,30 @@
+var userName= "null";
+if (userName='Jane') {
+    console.log("Hello" + " " + "userName");
+} else {
+    console.log("Hello!");
+}
+var userQuestion = "null"
+console.log(userQuestion);
+
+var randomNumber= Math.floor(Math.random() * 8);
+var eightBall ="null";
+
+if (randomNumber = 1 ) {
+    eightBall = 'It is certain';
+} else if (randomNumber = 2 ) {
+    eightBall = 'It is decidedly so';
+} else if (randomNumber = 3 ) {
+    eightBall = 'Reply hazy try again';
+} else if (randomNumber = 4) {
+    eightBall = 'Cannot predict now';
+} else if (randomNumber = 5) {
+    eightBall = 'Do not count on it';
+} else if (randomNumber = 6) {
+    eightBall = 'My sources say no';
+} else if (randomNumber = 7) {
+    eightBall = 'Outlook not so good';
+} else if (randomNumber = 8) {
+    eightBall = 'Signs point to yes';
+}
+console.log(eightBall);
